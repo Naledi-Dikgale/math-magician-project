0e40c8492be27171339d88413bae3dac3f19b249
naledi-dikgale <naledi22khosi@gmail.com>
2023-05-01 08:12:38 +0200

    Add home and navigation files and update styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter as Naledi, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import './index.css';
-// import Quotes from './components/quotes';
+import Quotes from './components/quotes';
+import Home from './components/Home';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <Calculator />
+    <Naledi>
+      <div>
+        <Navigation />
+        <div className="home">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/calculator" element={<Calculator />} />
+            <Route path="/quotes" element={<Quotes />} />
+          </Routes>
+        </div>
+      </div>
+    </Naledi>
   );
 }
 
---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+const Navigation = () => (
+  <nav className="nav-bar">
+    <h1 className="logo">Math Magicians</h1>
+    <ul className="nav-items">
+      <li><a className="line" href="/">Home</a></li>
+      <li><a className="line" href="/calculator">Calculator</a></li>
+      <li><a href="/quotes">Quotes</a></li>
+    </ul>
+  </nav>
+);
+export default Navigation;
