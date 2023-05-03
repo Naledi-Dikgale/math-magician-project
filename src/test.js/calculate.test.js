4d1a2f76e97a27b0274dce4f83045767b2b9ebb6
naledi-dikgale <naledi22khosi@gmail.com>
2023-05-03 14:45:57 +0200

    Resolve conflict

--- src/test.js/calculate.test.js
+++ src/test.js/calculate.test.js
@@ -10,17 +10,6 @@ describe('testing operate.js function', () => {
     expect(typeof calculate(obj, 'AC')).toBe('object');
   });
 
-<<<<<<< HEAD
-  test('expect 0 and obj.next to be 0', () => {
-    const obj = {
-      total: null,
-      next: '0',
-      operation: null,
-    };
-    expect(calculate(obj, '0')).toEqual({});
-  });
-});
-=======
     test('expect 0 and obj.next to be 0', () => {
         const obj = {
             total: null,
@@ -30,7 +19,7 @@ describe('testing operate.js function', () => {
         expect(calculate(obj, '0')).toEqual({});
     });
 
-    test('expect 0 and obj.next to be 0', () => {
+    test('expect 0 and obj.next to be empty object', () => {
         const obj = {
             total: null,
             next: null,
@@ -39,4 +28,3 @@ describe('testing operate.js function', () => {
         expect(calculate(obj, 'AC')).toEqual(obj);
     });
 })
->>>>>>> 246328c3522de47f8c6d02118c8e7a9f561c95e5
