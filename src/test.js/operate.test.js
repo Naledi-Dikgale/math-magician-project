e4be58cd9da2989eca5df9ef97012970b2e21288
naledi-dikgale <naledi22khosi@gmail.com>
2023-05-03 11:44:58 +0200

    Write unit tests for operate and calculate.js

--- src/test.js/operate.test.js
+++ src/test.js/operate.test.js
@@ -2,7 +2,11 @@ import React from  'react';
 import operate from '../logic/operate';
 
 describe('testing operate.js function', () => {
-    test('expecct tobe a function', () => {
-        expect(typeof operate()).toBe('function');
+    test('expect tobe a string', () => {
+        expect(typeof operate(2, 2, '+')).toBe('string');
+    });
+
+    test('expect 3 * 3 to be 9', () => {
+        expect(operate(3, 3, 'x')).toBe('9');
     })
 })
