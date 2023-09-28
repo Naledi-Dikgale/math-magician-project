5244ea66990acbd1312da2fe543105044f85b3c2
naledi-dikgale <naledi22khosi@gmail.com>
2023-09-28 15:28:29 +0200

    update styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,21 +5,13 @@ const Home = () => (
       Mathematical operations are elegant and universal building blocks
       of quantitative sciences. They simplify complex concepts into
       easily understood manipulations, allowing for new discoveries
-      and insights. Beyond numbers, they apply to various fields, modeling
-      and predicting the behavior of systems from the movement of
-      planets to the spread of diseases, and even optimizing complex
-      systems like transportation and finance.
-
+      and insights.
     </p>
     <p className="welcome-text">
       The beauty of mathematical operations lies in their simplicity,
       universality, and versatility. They are not only limited to numbers
       but can be applied to a wide range of fields, providing a powerful
-      tool for understanding and exploring the world around us. Mathematical
-      operations simplify complex concepts and allow for new discoveries,
-      insights, and innovations, revealing hidden patterns and relationships,
-      and opening up new possibilities for progress.
-
+      tool for understanding and exploring the world around us..
     </p>
   </div>
 );
